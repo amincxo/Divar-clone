99b39a57356778b85b223fe91093ed04e1211dfd
unknown <amincxo@gmail.com>
2024-10-14 01:31:08 +0330

    add defult config ro config for react query

---
+++ frontend/src/configs/reactQuery.js
@@ -0,0 +1,11 @@
+const defaultOptions = {
+        queries:{
+        refetchOnWindowFocus:false,
+        refetchOnMount:false,
+        retry:1,
+        staleTime:60*100,
+    },
+ };
+
+
+ export default defaultOptions
