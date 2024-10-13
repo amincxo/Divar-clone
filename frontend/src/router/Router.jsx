66481c93c201fb02a6d3c96eae702a8576319ff7
unknown <amincxo@gmail.com>
2024-10-14 01:57:15 +0330

    get role and data of user

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import { Route, Routes } from 'react-router-dom'
 
+import { getProfile } from 'services/user'
 import PageNotFound from 'src/pages/404'
 import AdminPage from 'src/pages/AdminPage'
 import AuthPage from 'src/pages/AuthPage'
@@ -8,7 +10,9 @@ import HomePage from 'src/pages/HomePage'
 
 
 function Router() {
-  return (
+    const {data , isLoading} = useQuery(["profile"], getProfile)
+    console.log({data , isLoading})
+    return (
     <Routes>
     <Route index element={<HomePage />} />
     <Route path="/dashboard" element={<Dashboard />} />
--- frontend/src/utils/cookie.js
+++ frontend/src/utils/cookie.js
@@ -5,6 +5,11 @@ const setCookie = tokens => {
     document.cookie = `refreshToken=${tokens.refreshToken}; max-age=${
         30 * 24 * 60 * 60
     }`
+};
+
+
+const getCookie = (cookieName) =>{
+    return document.cookie.split(";").find((token)=> token.trim().split("=")[0] === cookieName )?.split("=")[1];
 }
 
-export {setCookie}
+export {setCookie , getCookie }
