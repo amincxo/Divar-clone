6edd86ead55b768bb7b4cfaec1bb3a704c9f6d85
unknown <amincxo@gmail.com>
2024-10-14 02:42:05 +0330

    add configs api cookie and user router

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getCookie } from "utils/cookie";
 
 
 const api = axios.create({
@@ -8,5 +9,15 @@ const api = axios.create({
     }
 });
 
+
+api.interceptors.request.use((request)=> {
+    const accessToken = getCookie("accessToken");
+    if(accessToken) {
+        request.headers["Authorization"] = `bearer ${accessToken}` 
+    }
+    return request
+} , error => {
+    return Promise.reject(error);
+})
 
 export default api;
