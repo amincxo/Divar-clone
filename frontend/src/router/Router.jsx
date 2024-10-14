7d819bea1252ede642f7bf6e45d002d260118e0c
unknown <amincxo@gmail.com>
2024-10-14 07:57:04 +0330

    update routes and loadings

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
-import { Route, Routes } from 'react-router-dom'
-
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { getProfile } from 'services/user'
+
+import Loader from 'components/modules/Loader'
 import PageNotFound from 'src/pages/404'
 import AdminPage from 'src/pages/AdminPage'
 import AuthPage from 'src/pages/AuthPage'
@@ -12,13 +13,13 @@ import HomePage from 'src/pages/HomePage'
 function Router() {
     const {data , isLoading} = useQuery(["profile"], getProfile)
     console.log({data , isLoading})
-// if (isLoading) return <h1>loadingggg</h1>
+if (isLoading) return <Loader />
     return (
     <Routes>
     <Route index element={<HomePage />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/auth" element={<AuthPage />} />
-    <Route path="/admin" element={<AdminPage />} />
+    <Route path="/dashboard" element={data ? <Dashboard /> : <Navigate to="/auth" />} />
+    <Route path="/auth" element={ data ? <Navigate to="/dashboard"/> : <AuthPage /> } />
+    <Route path="/admin" element={ data && data.data.role =="ADMIN"? <AdminPage /> : <Navigate to="/" /> } />
     <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
