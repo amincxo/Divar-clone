0ad2f42ec590ee4ec3951cce8d5318061d99013d
unknown <amincxo@gmail.com>
2024-10-14 19:11:28 +0330

    add footer and header and styles

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+  return (
+    <footer className={styles.footer} >
+        <p>divar app develope by amin borvayeh</p>
+    </footer>
+  )
+}
+
+export default Footer
