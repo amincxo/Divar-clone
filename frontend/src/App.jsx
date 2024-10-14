2c9c9b6d2c815a12d9e790020338766a564a3c77
unknown <amincxo@gmail.com>
2024-10-14 19:12:20 +0330

    rap layout inside app update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter} from "react-router-dom";
 import Router from "router/Router";
 import defaultOptions from "configs/reactQuery";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "./layout/Layout";
 
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient} >
         <BrowserRouter>
-            <Router />
+            <Layout >
+                <Router />
+            </Layout>
         </BrowserRouter>
         <ReactQueryDevtools />
     </QueryClientProvider>
