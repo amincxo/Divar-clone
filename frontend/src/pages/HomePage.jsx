e74ab42c2e3320c85636dc6b74683dde0b0395bb
unknown <amincxo@gmail.com>
2024-10-16 02:46:13 +0330

    update home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import Loader from "components/modules/Loader"
+import Main from "components/templates/Main"
+import Sidebar from "components/templates/Sidebar"
+import { getCategory } from "services/admin"
+import { getAllPost } from "services/user"
+
+
+const style={
+    display:"flex"
+}
 
 
 function HomePage() {
+
+    const {data:posts , isLoading:postLoading} = useQuery(["post-list"], getAllPost);
+    const {data:categories , isLoading:categoriesloading} = useQuery(["get-categories"] , getCategory );
+
   return (
-    <div>HomePage</div>
+    <>
+    { postLoading || categoriesloading ? (
+        <Loader />
+    ) : (
+        <div style={style} >
+        <Sidebar categories={categories}  />
+        <Main posts={posts} />
+    </div>
+    )}
+    </>
   )
 }
 
