db43de53e504c486015388245850a2305f564f49
unknown <amincxo@gmail.com>
2024-10-15 21:53:07 +0330

    update and style category

--- frontend/src/components/templates/CategoryForm.jsx
+++ frontend/src/components/templates/CategoryForm.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react"
-import styles from "./Category.module.css"
+import styles from "./CategoryForm.module.css"
+import { useMutation , useQueryClient } from "@tanstack/react-query"
+import { addCategory } from "services/admin"
 
 
 function CategoryForm() {
+
+    const queryClient = useQueryClient()
     const [form , setForm]  = useState({name:"", slug:"" , icon:""})
+    
+    const {mutate , isLoading ,error ,data } = useMutation(addCategory ,
+        {
+            onSuccess: ()=> queryClient.invalidateQueries("get-categories")
+        }
+    )
+    
+    console.log({isLoading , error ,data})
+
     const changeHandler = (event) => {
         setForm({...form , [event.target.name]: event.target.value})
     }
@@ -11,19 +24,22 @@ function CategoryForm() {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        console.log(form)
+        if (!form.name|| !form.slug || !form.icon) {  return   }
+        mutate(form)
     }
 
   return (
     <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}  >  
             <h3>دسته بندی جدید</h3>
+            {!!error && <p>مشکلی پیش امئه کسگم</p>}
+            {data?.status === 201 && <p>دسته بندی با موفقیت اضافه شد</p>}
             <label htmlFor="name">نام دسته بندی</label>
             <input type="text" name="name" id="name"  />
             <label htmlFor="slug"> اسلاگ </label>
             <input type="text" name="slug" id="slug"  />
             <label htmlFor="icon"> ایکون </label>
             <input type="text" name="icon" id="icon"  />
-            <button type="submit" >ایجاد</button>
+            <button type="submit" disabled={isLoading} >ایجاد</button>
     </form>
   )
 }
